7a763c7938ca91e1cc4b0968dfbb4bcf42c532db
DeakinP <wizlak99@gmail.com>
2024-07-26 10:42:34 +1000

    Wk8 C++ Start

---
+++ Source/MGD_Template/Public/MGGameMode_Lobby.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MGGameMode.h"
+#include "MGGameMode_Lobby.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MGD_TEMPLATE_API AMGGameMode_Lobby : public AMGGameMode
+{
+	GENERATED_BODY()
+	
+};
