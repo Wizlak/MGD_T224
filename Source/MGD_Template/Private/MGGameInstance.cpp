d182520d5bb4d289cc05908d1e3c6c528c92ca56
DeakinP <wizlak99@gmail.com>
2024-08-01 09:18:51 +1000

    Finished Wk9 Recording

--- Source/MGD_Template/Private/MGGameInstance.cpp
+++ Source/MGD_Template/Private/MGGameInstance.cpp
@@ -5,6 +5,33 @@
 #include "OnlineSubsystem.h"
 #include "OnlineSubsystemUtils.h"
 
+void UMGGameInstance::Init()
+{
+	Super::Init();
+
+	// this is the online subsystem
+	// this gives us access to all of the online subsystem functions
+	const IOnlineSubsystem* ossRef = Online::GetSubsystem(GetWorld());
+
+	// if the ossRef returns null
+	// exit the function
+	if (!ossRef)
+		return;
+
+	// this is the identity interface for the EOS
+	// we can get info about the player's account for this interface
+	// it also allows us to access authentication (login / logout)
+	const IOnlineIdentityPtr identityRef = ossRef->GetIdentityInterface();
+
+	// if the identityRef returns null
+	// exit the function
+	if (!identityRef)
+		return;
+
+	// binding the login complete function to login complete in the identity interface
+	identityRef->OnLoginCompleteDelegates->AddUObject(this, &UMGGameInstance::EOSLoginComplete);
+}
+
 void UMGGameInstance::LoginEOS()
 {
 	// this is the online subsystem
@@ -35,4 +62,45 @@ void UMGGameInstance::LoginEOS()
 
 	// run the login portal
 	identityRef->Login(0, accCreds);
+}
+
+bool UMGGameInstance::IsLoggedIn() const
+{
+	// this is the online subsystem
+	// this gives us access to all of the online subsystem functions
+	const IOnlineSubsystem* ossRef = Online::GetSubsystem(GetWorld());
+
+	// if the ossRef returns null
+	// exit the function
+	if (!ossRef)
+		return false;
+
+	// this is the identity interface for the EOS
+	// we can get info about the player's account for this interface
+	// it also allows us to access authentication (login / logout)
+	const IOnlineIdentityPtr identityRef = ossRef->GetIdentityInterface();
+
+	// if the identityRef returns null
+	// exit the function
+	if (!identityRef)
+		return false;
+
+	return identityRef->GetLoginStatus(0) == ELoginStatus::LoggedIn;
+}
+
+FString UMGGameInstance::GetDisplayName() const
+{
+	if (!IsLoggedIn())
+		return FString(TEXT("DisplayName"));
+
+	// since is logged in already checks that this interface exists we can skip a step straight to the identity
+	const IOnlineIdentityPtr identityRef = Online::GetSubsystem(GetWorld())->GetIdentityInterface();
+
+	return identityRef->GetPlayerNickname(0);
+}
+
+void UMGGameInstance::EOSLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId,
+                                       const FString& Error)
+{
+	OnLoginComplete(bWasSuccessful, Error);
 }
--- Source/MGD_Template/Public/MGGameInstance.h
+++ Source/MGD_Template/Public/MGGameInstance.h
@@ -16,14 +16,25 @@ class MGD_TEMPLATE_API UMGGameInstance : public UGameInstance
 
 	// keyword that allows other classes to run this function
 public:
+	virtual void Init() override;
+
 	UFUNCTION(BlueprintCallable, Category="Login")
 	void LoginEOS();
 
+	UFUNCTION(BlueprintPure, Category="Login")
+	bool IsLoggedIn() const;
+
+	UFUNCTION(BlueprintPure, Category="User")
+	FString GetDisplayName() const;
+
 	// keyword that only allows this class and derived/child classes to run this function
 protected:
-
+	UFUNCTION(BlueprintImplementableEvent, Category="Login")
+	void OnLoginComplete(bool success, const FString& error);
+	
 	// only this class can run private functions
 private:
-	
+	// runs when EOS login completes
+	void EOSLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
 	
 };
